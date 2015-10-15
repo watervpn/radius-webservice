a9481d0fe4f22ac4be7b67a9fe971379789dd30a
Alfred <kanalfred@gmail.com>
2015-10-14 18:23:19 -0700

    code factoring

--- module/Lib/Module.php
+++ module/Lib/Module.php
@@ -36,64 +36,33 @@ class Module
                 },
 
                 /*
-                 * Mapper - tablegateway
+                 * Mapper 
                  */
                 // CheckMapper
                 'Lib\Radius\Mapper\Check' =>  function ($sm) {
                     $tableGateway = $sm->get('Lib\Radius\TableGateway\Check');
                     return new Radius\Mapper\Check($tableGateway);
                 },
-                'Lib\Radius\TableGateway\Check' =>  function ($sm) {
-                    $adapter = $sm->get('Db\Radius');
-                    $resultSetPrototype = new ResultSet();
-                    $resultSetPrototype->setArrayObjectPrototype(new Radius\Model\Check() );
-                    return new TableGateway('radcheck', $adapter, null, $resultSetPrototype);
-                },
                 // ReplyMapper
                 'Lib\Radius\Mapper\Reply' =>  function ($sm) {
                     $tableGateway = $sm->get('Lib\Radius\TableGateway\Reply');
                     return new Radius\Mapper\Reply($tableGateway);
-                },
-                'Lib\Radius\TableGateway\Reply' =>  function ($sm) {
-                    $adapter = $sm->get('Db\Radius');
-                    $resultSetPrototype = new ResultSet();
-                    $resultSetPrototype->setArrayObjectPrototype(new Radius\Model\Reply() );
-                    return new TableGateway('radreply', $adapter, null, $resultSetPrototype);
                 },
                 // GroupCheckMapper
                 'Lib\Radius\Mapper\GroupCheck' =>  function ($sm) {
                     $tableGateway = $sm->get('Lib\Radius\TableGateway\GroupCheck');
                     return new Radius\Mapper\GroupCheck($tableGateway);
                 },
-                'Lib\Radius\TableGateway\GroupCheck' =>  function ($sm) {
-                    $adapter = $sm->get('Db\Radius');
-                    $resultSetPrototype = new ResultSet();
-                    $resultSetPrototype->setArrayObjectPrototype(new Radius\Model\GroupCheck() );
-                    return new TableGateway('radgroupcheck', $adapter, null, $resultSetPrototype);
-                },
                 // GroupReplyMapper
                 'Lib\Radius\Mapper\GroupReply' =>  function ($sm) {
                     $tableGateway = $sm->get('Lib\Radius\TableGateway\GroupReply');
                     return new Radius\Mapper\GroupReply($tableGateway);
-                },
-                'Lib\Radius\TableGateway\GroupReply' =>  function ($sm) {
-                    $adapter = $sm->get('Db\Radius');
-                    $resultSetPrototype = new ResultSet();
-                    $resultSetPrototype->setArrayObjectPrototype(new Radius\Model\GroupReply() );
-                    return new TableGateway('radgroupreply', $adapter, null, $resultSetPrototype);
                 },
                 // UserGroupMapper
                 'Lib\Radius\Mapper\UserGroup' =>  function ($sm) {
                     $tableGateway = $sm->get('Lib\Radius\TableGateway\UserGroup');
                     return new Radius\Mapper\UserGroup($tableGateway);
                 },
-                'Lib\Radius\TableGateway\UserGroup' =>  function ($sm) {
-                    $adapter = $sm->get('Db\Radius');
-                    $resultSetPrototype = new ResultSet();
-                    $resultSetPrototype->setArrayObjectPrototype(new Radius\Model\UserGroup() );
-                    return new TableGateway('radusergroup', $adapter, null, $resultSetPrototype);
-                },
-
                 /*
                  * Mapper - composite of sub mappers
                  */
@@ -107,6 +76,46 @@ class Module
                     $groupCheckMapper = $sm->get('\Lib\Radius\Mapper\GroupCheck');
                     return new Radius\Mapper\Account($adapter, $entity, $checkMapper, $groupMapper, $groupCheckMapper);
                 },
+
+                /*
+                 * TableGateway
+                 */
+                // Check Tablegateway
+                'Lib\Radius\TableGateway\Check' =>  function ($sm) {
+                    $adapter = $sm->get('Db\Radius');
+                    $resultSetPrototype = new ResultSet();
+                    $resultSetPrototype->setArrayObjectPrototype(new Radius\Model\Check() );
+                    return new TableGateway('radcheck', $adapter, null, $resultSetPrototype);
+                },
+                // Reply Tablegateway
+                'Lib\Radius\TableGateway\Reply' =>  function ($sm) {
+                    $adapter = $sm->get('Db\Radius');
+                    $resultSetPrototype = new ResultSet();
+                    $resultSetPrototype->setArrayObjectPrototype(new Radius\Model\Reply() );
+                    return new TableGateway('radreply', $adapter, null, $resultSetPrototype);
+                },
+                // GroupCheck Tablegateway
+                'Lib\Radius\TableGateway\GroupCheck' =>  function ($sm) {
+                    $adapter = $sm->get('Db\Radius');
+                    $resultSetPrototype = new ResultSet();
+                    $resultSetPrototype->setArrayObjectPrototype(new Radius\Model\GroupCheck() );
+                    return new TableGateway('radgroupcheck', $adapter, null, $resultSetPrototype);
+                },
+                // Groupreply Tablegateway
+                'Lib\Radius\TableGateway\GroupReply' =>  function ($sm) {
+                    $adapter = $sm->get('Db\Radius');
+                    $resultSetPrototype = new ResultSet();
+                    $resultSetPrototype->setArrayObjectPrototype(new Radius\Model\GroupReply() );
+                    return new TableGateway('radgroupreply', $adapter, null, $resultSetPrototype);
+                },
+                // UserGroup Tablegateway
+                'Lib\Radius\TableGateway\UserGroup' =>  function ($sm) {
+                    $adapter = $sm->get('Db\Radius');
+                    $resultSetPrototype = new ResultSet();
+                    $resultSetPrototype->setArrayObjectPrototype(new Radius\Model\UserGroup() );
+                    return new TableGateway('radusergroup', $adapter, null, $resultSetPrototype);
+                },
+
 
                 /*
                  * Web Service Respondent 
